a415296d797c334b5670b47ae72085066dcf4787
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-03-16 04:32:05 +0530

    feat: add constant and types for navbar

---
+++ src/constants/index.ts
@@ -0,0 +1,47 @@
+import { NavLinksType } from '@/types';
+
+export const NAV_LINKS: NavLinksType = {
+  links: [
+    {
+      name: 'About',
+      url: '#about',
+    },
+    {
+      name: 'Courses',
+      url: '#courses',
+    },
+    {
+      name: 'Sponsorships',
+      url: '#sponsorships',
+    },
+    {
+      name: 'Blogs',
+      url: '/blogs',
+    },
+  ],
+  cta: {
+    name: 'Get in touch',
+    url: 'mailto:kunalkushwaha@wemakedevs.org',
+  },
+};
+
+export const NAV_LINKS_COURSE: NavLinksType = {
+  links: [
+    {
+      name: 'Courses',
+      url: '#courses',
+    },
+    {
+      name: 'FAQs',
+      url: '#faq',
+    },
+    {
+      name: 'Testimonials',
+      url: '#testimonials',
+    },
+  ],
+  cta: {
+    name: 'Get in touch',
+    url: 'mailto:kunalkushwaha@wemakedevs.org',
+  },
+};
---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+type NavLinkType = {
+  name: string;
+  url: string;
+};
+
+export type NavLinksType = {
+  links: NavLinkType[];
+  cta: NavLinkType;
+};
+
+export interface NavbarProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
+}
