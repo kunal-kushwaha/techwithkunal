24735729a92c9e593d0fab84293bc120e4174fd7
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-03-17 00:05:36 +0530

    feat: add assets for header sesction
    add header_image.webp
    add header_underline.svg
    add `@/assets/index.ts` to export all the events

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,5 @@
 import logoType from './logo_type.svg';
+import headerUnderline from './header_underline.svg';
+import headerImage from './header_image.webp';
 
-export { logoType };
+export { logoType, headerUnderline, headerImage };
