02a4fb6a3de97ed629ab6621e84b2f979f96dda2
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-03-19 00:37:31 +0530

    feat: add /blog path in next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://codewithkunal-blog.vercel.app/blog",
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://codewithkunal-blog.app/blog/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
