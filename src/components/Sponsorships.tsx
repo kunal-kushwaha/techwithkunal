0fe15958d5c45372ed763ad643ababc96f0e1f72
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-03-19 22:24:42 +0530

    feat: add new content in sponsorships section

--- src/components/Sponsorships.tsx
+++ src/components/Sponsorships.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/lib/utils';
 import { SponsorshipsProps } from '@/types';
 import { ViewContainer } from './ui/view-container';
 import { Icon } from './ui/icon';
-import { BarChart2, Mail, Smile, Sparkle, Zap } from 'lucide-react';
+import { BarChart2, HeartHandshake, LineChart, Rss, Zap } from 'lucide-react';
 import Link from 'next/link';
 import { buttonVariants } from './ui/button';
 
@@ -13,39 +13,43 @@ const Sponsorships = ({ className, ...props }: SponsorshipsProps) => {
       className={cn(className, 'py-16 md:py-20 lg:py-24')}
       {...props}
     >
-      <ViewContainer className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-14">
+      <ViewContainer className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         <div>
           <Icon>
             <BarChart2 />
           </Icon>
           <h2 className="text-3xl font-semibold mt-4 md:mt-6">
-            Growing brands — Sponsorships
+            Sponsorship opportunities
           </h2>
           <p className="text-gray-500 text-lg md:text-[20px] mt-3 md:mt-5">
-            Powerful and self-serving sponsorships to help you convert, engage
-            and retain more users.
+            As a passionate advocate for ethical community education and
+            fostering meaningful connections, I offer unique sponsorship
+            opportunities that benefit both your company and the broader
+            developer community.
           </p>
           <Link
-            href="/sponsorships"
+            href="mailto:kunal@wemakedevs.org"
             className={cn(
               buttonVariants({ variant: 'default', size: 'lg' }),
               'mt-8 md:mt-9 lg:mt-10'
             )}
           >
-            Sponsorships <Sparkle fill="currentColor" strokeWidth={0} />
+            Get in touch
           </Link>
         </div>
-        <div className="space-y-8 lg:space-y-14 md:row-start-2 lg:row-start-auto">
+        <div className="md:row-start-2 lg:row-start-auto col-span-1 md:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="rounded-md bg-gray-50 p-4 md:p-5 lg:p-6">
             <Icon variant="square">
-              <Mail />
+              <Rss />
             </Icon>
             <h3 className="text-lg md:text-[20px] font-medium mt-10 md:mt-12 lg:mt-16">
-              Share team inboxes
+              Dynamic Content Creation
             </h3>
             <p className="text-gray-500 text-base mt-2">
-              Whether you have a team of 2 or 200, our shared team inboxes keep
-              everyone on the same page and in the loop.
+              Leveraging my 600,000 YouTube subscribers, I create engaging video
+              content and write insightful blog posts, including newsletters.
+              These platforms are perfect for educating the community about your
+              projects and initiatives.
             </p>
           </div>
           <div className="rounded-md bg-gray-50 p-4 md:p-5 lg:p-6">
@@ -53,37 +57,39 @@ const Sponsorships = ({ className, ...props }: SponsorshipsProps) => {
               <Zap />
             </Icon>
             <h3 className="text-lg md:text-[20px] font-medium mt-10 md:mt-12 lg:mt-16">
-              Share team inboxes
+              Engaging Events
             </h3>
             <p className="text-gray-500 text-base mt-2">
-              Whether you have a team of 2 or 200, our shared team inboxes keep
-              everyone on the same page and in the loop.
-            </p>
-          </div>
-        </div>
-        <div className="space-y-8 lg:space-y-14 md:row-start-2 lg:row-start-auto">
-          <div className="rounded-md bg-gray-50 p-4 md:p-5 lg:p-6">
-            <Icon variant="square">
-              <BarChart2 />
-            </Icon>
-            <h3 className="text-lg md:text-[20px] font-medium mt-10 md:mt-12 lg:mt-16">
-              Share team inboxes
-            </h3>
-            <p className="text-gray-500 text-base mt-2">
-              Whether you have a team of 2 or 200, our shared team inboxes keep
-              everyone on the same page and in the loop.
+              I host a variety of events, both online and in-person, attended by
+              thousands. This includes webinars, podcasts, and more, offering
+              sponsors significant visibility and interaction opportunities with
+              an enthusiastic audience.
             </p>
           </div>
           <div className="rounded-md bg-gray-50 p-4 md:p-5 lg:p-6">
             <Icon variant="square">
-              <Smile />
+              <LineChart />
             </Icon>
             <h3 className="text-lg md:text-[20px] font-medium mt-10 md:mt-12 lg:mt-16">
-              Share team inboxes
+              Strategic Consulting
             </h3>
             <p className="text-gray-500 text-base mt-2">
-              Whether you have a team of 2 or 200, our shared team inboxes keep
-              everyone on the same page and in the loop.
+              With my experience in growing numerous open-source projects, I
+              provide consultancy services to attract quality contributors and
+              users, enhancing the visibility and impact of your projects.
+            </p>
+          </div>
+          <div className="rounded-md bg-gray-50 p-4 md:p-5 lg:p-6">
+            <Icon variant="square">
+              <HeartHandshake />
+            </Icon>
+            <h3 className="text-lg md:text-[20px] font-medium mt-10 md:mt-12 lg:mt-16">
+              DevRel Support
+            </h3>
+            <p className="text-gray-500 text-base mt-2">
+              I extend support to your Developer Relations efforts, aligning
+              with your team to maximize outreach and engagement, leveraging my
+              expertise to boost your community presence and impact.
             </p>
           </div>
         </div>
