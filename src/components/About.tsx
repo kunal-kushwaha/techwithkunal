e9cca1d117df855f1cee2c811a36835d103d0003
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-03-19 20:53:56 +0530

    feat: add new content of about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,55 +11,51 @@ const About = ({ className, ...props }: AboutProps) => {
       className={cn(className, 'my-20 md:my-28 lg:my-32')}
       {...props}
     >
-      <ViewContainer className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 lg:gap-14">
+      <ViewContainer className="grid grid-cols-1 lg:grid-cols-3 gap-8 md:gap-10 lg:gap-14">
         <div>
           <Icon>
             <Zap />
           </Icon>
           <h2 className="text-3xl font-semibold mt-4 md:mt-6">About me.</h2>
           <p className="text-gray-500 text-lg md:text-[20px] mt-3 md:mt-5">
-            Heyy there! I’m currently working as a DevRel @Civo. Other than
-            that, I’m a DevOps engineer, community builder and a super
-            instructor.
+            DevRel manager at Civo, CNCF Ambassador, GitHub Star
           </p>
         </div>
-        <div className="md:row-start-2 lg:row-start-auto space-y-6 md:space-y-8">
+        <div className="md:row-start-2 lg:row-start-auto md:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-10">
           <div>
             <h3 className="text-lg md:text-[20px] font-medium">
-              Empowering devs through courses
+              Educator and Mentor
             </h3>
             <p className="text-gray-500 text-base mt-2">
-              Whether you’re a beginner or an seasoned developer, my courses
-              will take you from nothing to everything.
+              Has educated thousands of developers, sharing in-depth knowledge
+              and expertise in tech.
             </p>
           </div>
           <div>
             <h3 className="text-lg md:text-[20px] font-medium">
-              DevOps engineer at the night{' '}
+              DevOps Consultant
             </h3>
             <p className="text-gray-500 text-base mt-2">
-              Started as a hobby, tinkering with devops is my passion now! I
-              have been working on cloud for the past 4 years.{' '}
-            </p>
-          </div>
-        </div>
-        <div className="md:row-start-2 lg:row-start-auto space-y-6 md:space-y-8">
-          <div>
-            <h3 className="text-lg md:text-[20px] font-medium">
-              Community builder during the day{' '}
-            </h3>
-            <p className="text-gray-500 text-base mt-2">
-              Looking to build a community around your product? You’ve come the
-              right place! I have built many developer centric communities.{' '}
+              Provides insightful consultancy to companies embarking on their
+              DevOps journeys.
             </p>
           </div>
           <div>
             <h3 className="text-lg md:text-[20px] font-medium">
-              Speaker during the weekends{' '}
+              Community Leader & Keynote Speaker
             </h3>
             <p className="text-gray-500 text-base mt-2">
-              I have spoken at various international tech conferences about
-              DevOps, community building and fun tech stuff!{' '}
+              Renowned for exceptional community leadership and engaging as a
+              speaker at global tech conferences.
+            </p>
+          </div>
+          <div>
+            <h3 className="text-lg md:text-[20px] font-medium">
+              Empathy and Professionalism
+            </h3>
+            <p className="text-gray-500 text-base mt-2">
+              Known for a unique blend of empathetic understanding and
+              professional acumen in his approach to work and life.
             </p>
           </div>
         </div>
