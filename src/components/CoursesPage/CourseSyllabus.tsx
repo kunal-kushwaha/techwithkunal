ed537bee8195b638b9ef41f345fd17460ff9fc82
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-04-11 00:42:51 +0530

    fix: typo in course syllabus header

--- src/components/CoursesPage/CourseSyllabus.tsx
+++ src/components/CoursesPage/CourseSyllabus.tsx
@@ -25,7 +25,7 @@ const CourseSyllabus = ({
             Syllabus
           </p>
           <h2 className="text-3xl font-semibold mt-1 md:mt-2 lg:mt-3">
-            Detailed syllabus design meeting industry standard
+            Detailed syllabus design meeting industry standards
           </h2>
           <p className="text-gray-500 text-lg md:text-[20px] mt-3 md:mt-5 max-w-full md:max-w-[75%] mx-auto">
             Here&apos;s a detailed overview of the course. Each topic is taught
