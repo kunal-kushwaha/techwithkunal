c4129bd9540a1256ac1f8726222405ccf1a4d693
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-10-21 22:49:47 +0530

    refactor: move all company testimonials and authors to a separate folder
    website doesn't work at this point, because the imports have not been updated

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,25 +2,6 @@ import logoType from './logo_type.svg';
 import logoTypeLight from './logo_type_light.svg';
 import headerUnderline from './header_underline.svg';
 import headerImage from './header_image.webp';
-import testimonialRiseinAuthor from './testimonial_risein_author.png';
-import testimonialRiseinCompany from './testimonial_risein_company.svg';
-import testimonialTeleportAuthor from './testimonial_teleport_author.png';
-import testimonialTeleportCompany from './testimonial_teleport_company.svg';
-import testimonialStreamAuthor from './testimonial_stream_author.png';
-import testimonialStreamCompany from './testimonial_stream_company.svg';
-import testimonialKubesphereAuthor from './testimonial_kubesphere_author.png';
-import testimonialKubeshphereAuthor2 from './testimonial_kubesphere_author2.png';
-import testimonialKubesphereCompany from './testimonial_kubesphere_company.svg';
-import testimonialBootdevCompany from './testimonial_bootdev_company.png';
-import testimonialBootdevAuthor from './testimonial_bootdev_author.png';
-import testimonialKodecloudCompany from './testimonial_kodecloud_company.svg';
-import testimonialKodecloudAuthor from './testimonial_kodecloud_author.png';
-import testimonialKomodorCompany from './testimonial_komodor_company.svg';
-import testimonialKomodorAuthor from './testimonial_komodor_author.png';
-import testimonialSlashdataCompany from './testimonial_slashdata_company.svg';
-import testimonialSlashdataAuthor from './testimonial_slashdata_author.png';
-import testimonialWilcoCompany from './testimonial_wilco_company.png';
-import testimonialWilcoAuthor from './testimonial_wilco_author.png';
 import companyGoogle from './company_google.svg';
 import companyCisco from './company_cisco.svg';
 import companyRazorpay from './company_razorpay.svg';
@@ -49,25 +30,6 @@ export {
   logoTypeLight,
   headerUnderline,
   headerImage,
-  testimonialStreamAuthor,
-  testimonialStreamCompany,
-  testimonialTeleportCompany,
-  testimonialTeleportAuthor,
-  testimonialRiseinAuthor,
-  testimonialRiseinCompany,
-  testimonialKubesphereAuthor,
-  testimonialKubesphereCompany,
-  testimonialKubeshphereAuthor2,
-  testimonialBootdevAuthor,
-  testimonialBootdevCompany,
-  testimonialKodecloudAuthor,
-  testimonialKodecloudCompany,
-  testimonialKomodorAuthor,
-  testimonialKomodorCompany,
-  testimonialSlashdataAuthor,
-  testimonialSlashdataCompany,
-  testimonialWilcoAuthor,
-  testimonialWilcoCompany,
   companyAdobe,
   companyClicklab,
   companyCisco,
---
+++ src/assets/testimonials/index.ts
@@ -0,0 +1,50 @@
+import bootdevLogo from './bootdev_company.png'
+import bootdevAuthor from './bootdev_author.png'
+
+import kodecloudLogo from './kodecloud_company.svg'
+import kodecloudAuthor from './kodecloud_author.png'
+
+import kubesphereLogo from './kubesphere_company.svg'
+import kubesphereAuthor from './kubesphere_author.png'
+import kubesphereAuthor2 from './kubesphere_author2.png'
+
+import teleportLogo from './teleport_company.svg'
+import teleportAuthor from './teleport_author.png'
+
+import wilcoLogo from './wilco_company.png'
+import wilcoAuthor from './wilco_author.png'
+
+import komodorLogo from './komodor_company.svg'
+import komodorAuthor from './komodor_author.png'
+
+import slashdataLogo from './slashdata_company.svg'
+import slashdataAuthor from './slashdata_author.png'
+
+import riseinLogo from './risein_company.svg'
+import riseinAuthor from './risein_author.png'
+
+import streamLogo from './stream_company.svg'
+import streamAuthor from './stream_author.png'
+
+
+export {
+    bootdevAuthor,
+    bootdevLogo,
+    kodecloudAuthor,
+    kodecloudLogo,
+    kubesphereAuthor,
+    kubesphereLogo,
+    teleportAuthor,
+    teleportLogo,
+    wilcoAuthor,
+    wilcoLogo,
+    komodorAuthor,
+    komodorLogo,
+    kubesphereAuthor2,
+    riseinAuthor,
+    riseinLogo,
+    slashdataAuthor,
+    slashdataLogo,
+    streamAuthor,
+    streamLogo
+}
