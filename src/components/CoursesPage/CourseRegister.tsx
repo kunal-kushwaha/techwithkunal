dcc46e735bf325ab5c500503f8c5587cda66c866
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2025-11-21 00:34:03 +0530

    fix: update course register height according to new embed
    fixes #32

--- src/components/CoursesPage/CourseRegister.tsx
+++ src/components/CoursesPage/CourseRegister.tsx
@@ -25,12 +25,12 @@ const CourseRegister = ({
 				<div className="flex justify-center items-center py-4">
 					<iframe
 						src={
-							"https://techwithkunal-register.vercel.app/main.html"
+							"https://718dd6be.sibforms.com/serve/MUIFAJv0OM4EdoXdJyo1BVXjIyP-MUNwlWJOdHMBhRcZAPkgYync_I42pl-IIdHguJocapG-pu9X-Q3dDJ-diCsi6qp0aMEJGC222WYInL2V6TMRXcAGQZy0stHXmzR2quFjjrKvUv1A5H9FGQpnpTM8CUFQbAKaKrd73DiAqrP2Twivb1Do2y0RKkGnOlI4y-hn2Y2xSdZOj8HG"
 						}
 						width="100%"
 						scrolling="no"
 						className="border-none overflow-hidden"
-						height={400}
+						height={350}
 						title="Register in the course now!"
 					/>
 				</div>
